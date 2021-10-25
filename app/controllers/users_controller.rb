585aa7f1df4cafb9286615656e172ff4f18deb73
EC2 Default User <m3n7m3.97.415@gmail.com>
2021-10-25 08:15:20 +0000

    5章完成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,21 @@
 class UsersController < ApplicationController
   def show
+    @user = user.find(parms[:id])
+    @post_images = @user.post_images.page(params[:page]).reverse_order
   end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = user.find(params[:id])
+    @user = user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+  private
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
+  end
+  
 end
